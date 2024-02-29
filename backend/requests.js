c3ca1ac36d81a37761de582a7e603d66559e3cc9
Najel Alarcon <najelalarcon@gmail.com>
2024-02-28 20:03:02 -0800

    Started on backend

---
+++ backend/requests.js
@@ -0,0 +1,15 @@
+// Consider separate file later
+app.get('/login', async (req, res) => {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.CLIENT_ID,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
---
+++ backend/server.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+
+const express = require('express');
+const querystring = require('querystring');
+const PORT = 3000;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+function generateRandomString(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+
+// listens for requests on port 3000
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
+
+var redirect_uri = 'http://localhost:8888/callback';
+app.get('/login', async (req, res) => {
+
+    var state = generateRandomString(16);
+    var scope = 'user-read-private user-read-email';
+  
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.CLIENT_ID,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      }));
+});
+
+
+app.get('/callback', async (req, res) => {
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+  
+    if (state === null) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
+    }
+});
+  
