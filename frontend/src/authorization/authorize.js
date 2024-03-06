9fb5a8cc83cc2a536106a091247708f276986bef
Najel-A <59577073+Najel-A@users.noreply.github.com>
2024-03-06 15:40:47 -0800

    Got to authorize spotify account

--- frontend/src/authorization/authorize.js
+++ frontend/src/authorization/authorize.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const clientId = 'yourClientIDGoesHere'; // your clientId
-const redirectUrl = 'http://localhost:8080'; // your redirect URL - must be localhost URL and/or HTTPS
+const clientId = process.env.REACT_APP_CLIENT_ID; // your clientId
+const redirectUrl = process.env.REACT_APP_REDIRECT_URL; // your redirect URL - must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
@@ -54,8 +54,34 @@ function Authorize() {
     return await response.json();
   };
 
-  const redirectToSpotifyAuthorize = () => {
-    // Implement your code here
+  const redirectToSpotifyAuthorize = async () => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const randomValues = crypto.getRandomValues(new Uint8Array(64));
+    const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
+
+    const code_verifier = randomString;
+    const data = new TextEncoder().encode(code_verifier);
+    const hashed = await crypto.subtle.digest('SHA-256', data);
+
+    const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+
+    window.localStorage.setItem('code_verifier', code_verifier);
+
+    const authUrl = new URL(authorizationEndpoint)
+    const params = {
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      code_challenge_method: 'S256',
+      code_challenge: code_challenge_base64,
+      redirect_uri: redirectUrl,
+    };
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString(); // Redirect the user to the authorization server for login
   };
 
   const refreshToken = async () => {
@@ -63,7 +89,12 @@ function Authorize() {
   };
 
   const getUserData = async () => {
-    // Implement your code here
+    const response = await fetch("https://api.spotify.com/v1/me", {
+    method: 'GET',
+    headers: { 'Authorization': 'Bearer ' + currentToken.access_token },
+  });
+
+    return await response.json();
   };
 
   const loginWithSpotifyClick = async () => {
